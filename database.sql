53f46ebfd410b920049eb7e81a110c67e9b4edd4
01-akm <kraadi4941@gmail.com>
2025-08-28 14:00:33 +0530

    Set 'form_db' as the active database
    Set 'form_db' as the active database

--- database.sql
+++ database.sql
@@ -6,3 +6,5 @@ DROP DATABASE IF EXISTS `form_db`;
 -- Create a new database named 'form_db'.
 -- The character set and collation are set to handle a wide range of characters, including international ones.
 CREATE DATABASE `form_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Switch to the newly created database so the following commands apply to it.
+USE `form_db`;
