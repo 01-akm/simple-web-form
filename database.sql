a9098d84cba6d9b8094fdce11d5d4d1ef8c21319
01-akm <kraadi4941@gmail.com>
2025-08-28 14:00:00 +0530

    Create 'form_db' database with utf8mb4 character set
    Create 'form_db' database with utf8mb4 character set

--- database.sql
+++ database.sql
@@ -3,3 +3,6 @@
 -- It's good practice to check if the database already exists to avoid errors.
 -- If it exists, it will be dropped and a new one will be created.
 DROP DATABASE IF EXISTS `form_db`;
+-- Create a new database named 'form_db'.
+-- The character set and collation are set to handle a wide range of characters, including international ones.
+CREATE DATABASE `form_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
