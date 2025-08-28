303427dd9c801154acbf20c5409666b16948dcc4
01-akm <kraadi4941@gmail.com>
2025-08-28 13:58:40 +0530

    Add command to drop database if it exists

---
+++ database.sql
@@ -0,0 +1,5 @@
+-- This script creates the database and the table needed for the web form.
+
+-- It's good practice to check if the database already exists to avoid errors.
+-- If it exists, it will be dropped and a new one will be created.
+DROP DATABASE IF EXISTS `form_db`;
