9888076df08473c94632e29269603f7418103fe8
01-akm <kraadi4941@gmail.com>
2025-08-28 13:01:48 +0530

    Add check to ensure script runs only on POST request
     Add check to ensure script runs only on POST request

--- submit.php
+++ submit.php
@@ -6,3 +6,7 @@ $servername = "localhost"; // Usually 'localhost' or an IP address
 $username = "root";        // Your database username
 $password = "";            // Your database password
 $dbname = "form_db";       // The name of the database we created
+
+// Create a new MySQLi connection.
+// The '@' symbol suppresses default PHP errors, allowing us to handle them manually.
+$conn = @new mysqli($servername, $username, $password, $dbname);
